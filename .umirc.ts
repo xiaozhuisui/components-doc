feef7cad5031bb18538ed59fc4c51cd586ec5fa2
比升-郑圳宏 <zhenhong.zzh@bitsun-inc.com>
2022-08-31 11:49:57 +0800

    添加部署文件

--- .umirc.ts
+++ .umirc.ts
@@ -1,3 +1,8 @@
+/*
+ * @Date: 2022-08-31 11:04:25
+ * @LastEditors: 追随
+ * @LastEditTime: 2022-08-31 11:49:34
+ */
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
@@ -7,5 +12,20 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  base: '/xiaozhuisui',
+  publicPath: '/xiaozhuisui/',
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'lean',
+        camel2DashComponentName: false,
+        customStyleName: name => {
+          return `./style/index.less`; // 注意：这里 ./ 不可省略
+        },
+      },
+      'lean',
+    ],
+  ]
   // more config: https://d.umijs.org/config
 });
